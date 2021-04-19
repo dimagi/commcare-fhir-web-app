09f0a241995624ad37519b48c85b9e2a7885315e
Norman Hooper <nhooper@dimagi.com>
2021-04-19 20:37:25 +0200

    Added views

---
+++ web_app/fhir_client.py
@@ -0,0 +1,2 @@
+async def fetch_patient(patient_id):
+    return {}  # TODO: Fetch FHIR Patient
--- web_app/main.py
+++ web_app/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+from web_app.fhir_client import fetch_patient
+
+app = FastAPI(default_response_class=HTMLResponse)
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return templates.TemplateResponse("root.html", {
+        'request': request,
+    })
+
+
+@app.get("/patient/")
+async def view_patient(request: Request):
+    patient_id = request.query_params['patient_id']
+    patient = await fetch_patient(patient_id)
+    return templates.TemplateResponse("patient.html", {
+        'request': request,
+        'patient': patient,
+    })
